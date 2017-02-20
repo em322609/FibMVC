6d000053353a596bda3ff65b189b87943554f611
em322609 <em322609@ohio.edu>
2017-02-20 11:46:24 -0500

    View update

---
+++ FibMVC/FibMVC/Controllers/MainController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FibMVC.Biz;
+
+namespace FibMVC.Controllers
+{
+    public class MainController : Controller
+    {
+        // GET: Main
+        public ActionResult Index()
+        {
+            List<int> fifteenFibs = new List<int>();
+            Fib fibInstance = new Fib();
+
+            fifteenFibs = fibInstance.returnListOfFib(15);
+            ViewData["fifteenFibs"] = fifteenFibs;
+
+            return View();
+        }
+    }
+}
