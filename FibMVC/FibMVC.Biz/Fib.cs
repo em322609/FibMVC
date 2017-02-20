19008cae5aca9470b85d2cf8da0d32a92b360feb
em322609 <em322609@ohio.edu>
2017-02-20 10:58:23 -0500

    FibMVC.Biz added to calculate fibonacci numbers

---
+++ FibMVC/FibMVC.Biz/Fib.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibMVC.Biz
+{
+    public class Fib
+    {
+        public List<int> fibList = new List<int>();
+        
+        public List<int> returnListOfFib(int num)
+        {
+            fibList.Add(0);
+            fibList.Add(1);
+            for(int i = 2; i < num; i++)
+            {
+                fibList.Add(fibList[i - 2] + fibList[i - 1]);
+            }
+
+            return fibList;
+        }
+
+    }
+}
